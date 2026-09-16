6cb7ceda44bf33f6a17ce826fd0c16ef2ffec567
Girish <girishsortur@gmail.com>
2026-09-16 23:30:45 +0900

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // Solution 1 - Use map to store the count of characters and compare the maps later. O(n+m) time and space
+        // Solution 2 - Use count in alphabet array with 26 chars and add count of repeated chars. O(n+m) time and O(1) space
+        int lenS = s.length();
+        int lenT = t.length();
+        if (lenS != lenT) {
+            return false;
+        }
+        int[] charCounter = new int[26];
+        int alphabetLowerValue = 97;
+        for (int i=0; i<lenS; i++) {
+            charCounter[s.charAt(i) - alphabetLowerValue]++;
+            charCounter[t.charAt(i) - alphabetLowerValue]--;
+        }
+        for (int i=0; i<charCounter.length; i++) {
+            if (charCounter[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
