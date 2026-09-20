f176e9bb64c379251d0bd93ad56d3153966dfc4d
Girish <girishsortur@gmail.com>
2026-09-20 23:37:19 +0900

    Add: two-integer-sum - submission-15

---
+++ Data Structures & Algorithms/two-integer-sum/submission-15.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Solution 1 - Use 2 for loops to obtain the numbers. O(n2) time and O(1) space
+        // Solution 2 - Sort array. Use 2 pointers and navigate the array until the sum is found. O(nlogn) time and O(1) space
+        // Solution 3 - Use map and store all elements. Subtract total minus the number at index to find final value. O(n) time and space
+        Map<Integer, Integer> itemMap = new HashMap<>();
+        int[] resultArray = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (itemMap.containsKey(diff)) {
+                resultArray[0] = itemMap.get(diff);
+                resultArray[1] = i;
+                break;
+            } else {
+                itemMap.put(nums[i], i);
+            }
+        }
+        return resultArray;
+    }
+}
